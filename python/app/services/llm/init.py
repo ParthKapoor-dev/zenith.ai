630dde1d035c4789ae2c7544df016b430be5caba
Parth Kapoor <parthkapoor.coder@gmail.com>
2025-02-07 19:22:37 +0530

    Using Hugging Face Mistral-7B for now

--- python/app/services/llm/init.py
+++ python/app/services/llm/init.py
@@ -1,21 +1,21 @@
 from langchain_ollama import OllamaLLM
 from langchain_huggingface import HuggingFaceEndpoint
 from app.config import HF_API_KEY
-# from langchain_core.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_core.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-llm = OllamaLLM(
-    model="mistral",
-    temperature=0
-)
-
-# callbacks = [StreamingStdOutCallbackHandler()]
-
-# llm = HuggingFaceEndpoint(
-#     endpoint_url="https://api-inference.huggingface.co/models/meta-llama/Llama-3.1-8B-Instruct",  
-#     huggingfacehub_api_token=HF_API_KEY,
-#     temperature=0.1,
-#     max_new_tokens=512,
-#     streaming=True,
-#     # callbacks=callbacks
-#     # Removed model_kwargs={"stop": ["\n"]}
+# llm = OllamaLLM(
+#     model="mistral",
+#     temperature=0
 # )
+
+callbacks = [StreamingStdOutCallbackHandler()]
+
+llm = HuggingFaceEndpoint(
+    endpoint_url="https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3", 
+    huggingfacehub_api_token=HF_API_KEY,
+    temperature=0.1,
+    max_new_tokens=512,
+    streaming=True,
+    callbacks=callbacks
+    # Removed model_kwargs={"stop": ["\n"]}
+)
