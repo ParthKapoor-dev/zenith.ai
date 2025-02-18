66e349ea29b924988fac84a2db577de1b60ecc1d
Parth Kapoor <parthkapoor.coder@gmail.com>
2025-02-18 18:45:59 +0530

    Minor Update

--- python/app/services/llm/init.py
+++ python/app/services/llm/init.py
@@ -3,19 +3,19 @@ from langchain_huggingface import HuggingFaceEndpoint
 from app.config import HF_API_KEY
 from langchain_core.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-llm = OllamaLLM(
-    model="mistral",
-    temperature=0
-)
-
-# callbacks = [StreamingStdOutCallbackHandler()]
-
-# llm = HuggingFaceEndpoint(
-#     endpoint_url="https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3", 
-#     huggingfacehub_api_token=HF_API_KEY,
-#     temperature=0.1,
-#     max_new_tokens=512,
-#     streaming=True,
-#     callbacks=callbacks
-#     # Removed model_kwargs={"stop": ["\n"]}
+# llm = OllamaLLM(
+    # model="mistral",
+    # temperature=0
 # )
+
+callbacks = [StreamingStdOutCallbackHandler()]
+
+llm = HuggingFaceEndpoint(
+    endpoint_url="https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3", 
+    huggingfacehub_api_token=HF_API_KEY,
+    temperature=0.1,
+    max_new_tokens=512,
+    streaming=True,
+    callbacks=callbacks
+    # Removed model_kwargs={"stop": ["\n"]}
+)
